e625cf79fe0ea4029284ab507f5c12d34dc18499
Alex <hjy@rmsoft.kr>
2023-01-13 14:13:02 +0900

    Add: menu 페이지 추가

---
+++ pages/views/menu/menuPage.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const FlexDiv = styled.div`
+  display: flex;
+`;
+
+const FlexColumnDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Menu = () => {
+  return <></>;
+};
+
+export default Menu;
